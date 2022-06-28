91799286cceedfabd604d47ed595305622cff551
ranggakorompis <korompisrangga14@gmail.com>
2022-06-28 22:17:24 +0800

    Buat komponen About

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section className="about">
+      <h2 className="subtitle">About</h2>
+      <h3>
+        I'm a student at Universitas Klabat. I specialise in Web Development.
+        I'm passionate about programming and I always striving to grow and learn
+        something new.
+      </h3>
+    </section>
+  );
+}
