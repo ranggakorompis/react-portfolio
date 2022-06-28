34e3f6e0ad33aabd9fadace73be914ebd2b7cfd9
ranggakorompis <korompisrangga14@gmail.com>
2022-06-28 22:32:06 +0800

    Import komponen Contact

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 // Import components
 import About from "./About";
 import Skills from "./Skills";
+import Contact from "./Contact";
 
 // Export function Main
 export default function Main() {
@@ -8,6 +9,7 @@ export default function Main() {
     <div className="main">
       <About />
       <Skills />
+      <Contact />
     </div>
   );
 }
