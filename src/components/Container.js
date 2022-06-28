d059be73e0351396b1984aaae52fb4cbdb4b55dd
ranggakorompis <korompisrangga14@gmail.com>
2022-06-28 22:17:51 +0800

    Import komponen Main dan About

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,13 @@
 // Import component
 import Header from "./Header";
+import Main from "./Main";
 
 // Export function Container
 export default function Container() {
   return (
     <div className="container">
       <Header />
+      <Main />
     </div>
   );
 }
