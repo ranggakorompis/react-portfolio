9d75eb39fd3c5d8e459679c3260c4b62f0c5df94
ranggakorompis <korompisrangga14@gmail.com>
2022-06-28 22:16:53 +0800

    Buat komponen Main

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+// Import component About
+import About from "./About";
+
+// Export function Main
+export default function Main() {
+  return (
+    <div className="main">
+      <About />
+    </div>
+  );
+}
