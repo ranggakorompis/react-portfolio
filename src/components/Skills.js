ebc3ab237ee63d7ed668c957ac3dd3e26e7e86fd
ranggakorompis <korompisrangga14@gmail.com>
2022-06-28 22:27:03 +0800

    Buat komponen Skills

---
+++ src/components/Skills.js
@@ -0,0 +1,27 @@
+export default function Skills() {
+  return (
+    <section className="skills">
+      <h2 className="subtitle">Skills</h2>
+      <ul>
+        <li>
+          <h3>HTML</h3>
+        </li>
+        <li>
+          <h3>CSS</h3>
+        </li>
+        <li>
+          <h3>Javascript</h3>
+        </li>
+        <li>
+          <h3>Bootstrap</h3>
+        </li>
+        <li>
+          <h3>React JS</h3>
+        </li>
+        <li>
+          <h3>Basic Network</h3>
+        </li>
+      </ul>
+    </section>
+  );
+}
