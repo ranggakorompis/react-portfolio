850a1ef56d2084189b8fa36a3ce05b97f9241170
ranggakorompis <korompisrangga14@gmail.com>
2022-06-30 01:05:15 +0800

    Menambahkan komponen ke json server

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,26 +1,27 @@
+// Import useState, useEffect
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Skills() {
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3004/skills")
+      .then((res) => setSkills(res.data));
+  });
+
   return (
     <section className="skills">
       <h2 className="subtitle">Skills</h2>
       <ul>
-        <li>
-          <h3>HTML</h3>
-        </li>
-        <li>
-          <h3>CSS</h3>
-        </li>
-        <li>
-          <h3>Javascript</h3>
-        </li>
-        <li>
-          <h3>Bootstrap</h3>
-        </li>
-        <li>
-          <h3>React JS</h3>
-        </li>
-        <li>
-          <h3>Basic Network</h3>
-        </li>
+        {skills.map((item) => {
+          return (
+            <li>
+              <h3>{item.skill}</h3>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
