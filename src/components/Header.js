129021cdc68873f53f39e9bd9068ab9927dd081b
ranggakorompis <korompisrangga14@gmail.com>
2022-06-30 00:48:07 +0800

    Menambahkan komponen ke json server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
-// Import component
-import heroimg from "../hero-img.jpg";
+// Import useState, useEffect, hero image
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import heroImg from "../hero-img.jpg";
 
 // Export function Header
 export default function Header() {
+  const [header, setHeader] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3004/header")
+      .then((res) => setHeader(res.data));
+  });
+
   return (
     <header className="header">
-      <div className="hero">
-        <div className="hero-img">
-          <img src={heroimg} alt="Hero Images" />
-        </div>
-        <h1 className="title">Rangga Aldo Korompis</h1>
-        <h2 className="tagline">Student</h2>
-      </div>
+      {header.map((item) => {
+        return (
+          <div className="hero">
+            <div className="hero-img">
+              <img src={heroImg} alt="Hero Images" />
+            </div>
+            <h1 className="title">{item.title}</h1>
+            <h2 className="tagline">{item.tagline}</h2>
+          </div>
+        );
+      })}
     </header>
   );
 }
